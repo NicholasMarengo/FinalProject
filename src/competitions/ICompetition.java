074ce309e49d6ece4447970ec4d21ceeee9c761d
NicholasMarengo <marengon13@gmail.com>
2014-12-04 20:30:07 -0500

    FinalProj
    Initial push

---
+++ src/competitions/ICompetition.java
@@ -0,0 +1,31 @@
+package competitions;
+
+import Event;
+import Team;
+
+/**
+ * The ICompetition interface provides the contract for exploiters to retrieve information about a competition.
+ */
+public interface ICompetition {
+
+    /**
+     * Retrieve the event that this competition is taking place
+     *
+     * @return the event that this competition is taking place
+     */
+    public Event getEvent();
+
+    /**
+     * Retrieve the home team taking part in this competition
+     *
+     * @return the home team taking part in this competition
+     */
+    public Team getHomeTeam();
+
+    /**
+     * Retrieve the away team taking part in this competition
+     *
+     * @return the away team taking part in this competition
+     */
+    public Team getAwayTeam();
+}
